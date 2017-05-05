ea9571aae6e15b016485af9df32459f82c1e9e01
Mattyx <mattyx22@gmail.com>
2017-05-05 17:13:17 +0200

    Repaired bugs and done some other things

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// Loader of the page 
+$(window).on('load', function() {
+	console.log('Strona zaladowana');
+	$('#loading_icon').fadeOut();
+	$('#preloader').delay(500).fadeOut('slow');
+	$('main, #slide_1').delay(350).show('slow');
+	$('html, body').animate({
+		scrollTop: 0
+	}, 500);
+});
+
+// When document is ready hide scrollbar
+$(document).ready(function(){
+	$('body').css('overflow-y', 'hidden');
+	
+});
+
+// Page sliding 
+var page = 1;
+var howManyPages = $('section').length;
+var sectionHeight = $('section').height();
+var nextPage;
+var previousPage ;
+var next;
+var previous;
+
+$(document).on('wheel', _.throttle(function(event){
+	next = page + 1;
+	previous = page - 1;
+	nextPage = '#slide_' + next;
+	previousPage = '#slide_' + previous;
+		
+	if (event.originalEvent.deltaY< 0 && previous != 0) {
+		$('html, body').animate({
+			scrollTop: $(previousPage).offset().top
+		}, 500);
+		
+		page = page - 1;
+	}
+	else if (event.originalEvent.deltaY > 0 && page != howManyPages){
+		$('html, body').animate({
+			scrollTop: $(nextPage).offset().top
+		}, 500);
+		
+		page = page + 1;
+	}
+	
+}, 1000));
